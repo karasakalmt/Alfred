a5544097a4095d2eea7d09706674c75daf5f365e
Mete Karasakal <mete@Metes-MacBook-Air.local>
2022-06-10 16:22:52 +0200

    file structure navigation and pages

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter,
+  Route,
+  Routes
+} from "react-router-dom";
+import Proposal from './components/Proposal';
+import Proposals from './components/Proposals';
+import CreateProposal from './components/CreateProposal';
+import Header from './components/Header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path="/" element={<App />}/>
+      <Route path="proposals" element={<Proposals />}/>
+      <Route path="proposals/proposal/:proposalId" element= {<Proposal />}/>
+      <Route path="create-a-proposal" element={<CreateProposal />}/>
+    </Routes>
+  </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
