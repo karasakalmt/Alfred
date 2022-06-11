5df4796d0e88f76d1fc8c395d7edf96841841f4a
Mete Karasakal <mete@Metes-MacBook-Air.local>
2022-06-11 02:51:09 +0200

    walletconnect removed rainbowkit added

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,50 +1,9 @@
 import { GiCrossedChains } from 'react-icons/gi'
 import { Link } from 'react-router-dom';
 import logo from '../logo_with_name.png';
-import { useWeb3 } from '../utils/web3ModalContext';
-import { useState, useEffect } from 'react';
+import { ConnectButton } from '@rainbow-me/rainbowkit';
 
 const Header = () => {
-    const web3 = useWeb3();
-    const [connected, setConnected] = useState(false);
-
-    useEffect(() => {
-        if (connected) {
-            const accessToken = localStorage.getItem('access_token');
-            const accessTokenExpiry = Number(localStorage.getItem('access_token_expires_at'));
-            const now = Date.now();
-            if (!accessToken || accessToken.length === 0 || now > accessTokenExpiry) {
-            localStorage.removeItem('access_token');
-            localStorage.removeItem('access_token_expires_at');
-            } else {
-            console.log('token exists and not expired')
-            }
-        }
-        }, [connected]);
-
-    useEffect(() => {
-    if (web3.account) {
-        setConnected(true);
-    } else {
-        setConnected(false);
-    }
-    }, [web3.account]);
-
-    const connectToWallet = async () => {
-        if (connected) {
-            await web3.clearCachedProvider();
-            setConnected(false);
-        } else {
-            try {
-            await web3.connect();
-            setConnected(true);
-            } catch (err) {
-            console.log(err);
-            setConnected(false);
-            }
-        }
-    }
-
     return(
         <nav>
             <div className="nav-left">
@@ -55,16 +14,7 @@ const Header = () => {
                     <li><Link to="/proposals">Proposals</Link></li>
                     <li><Link to="/create-a-proposal">Create a Proposal</Link></li>
                 </ul>
-                <button className="connect-wallet-button" onClick={connectToWallet}> <GiCrossedChains/> 
-                    {connected ? (
-                            "Connected: " +
-                            String(web3.account).substring(0, 6) +
-                            "..." +
-                            String(web3.account).substring(38)
-                        ) : (
-                            <span>Connect Wallet</span>
-                    )} 
-                </button>
+                <ConnectButton label={<div><GiCrossedChains/> Connect Wallet</div>}/>
             </div>
         </nav>
     )
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -11,18 +11,63 @@ import Proposal from './components/Proposal';
 import Proposals from './components/Proposals';
 import CreateProposal from './components/CreateProposal';
 import Header from './components/Header';
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  darkTheme,
+} from '@rainbow-me/rainbowkit';
+import {
+  chain,
+  configureChains,
+  createClient,
+  WagmiConfig,
+} from 'wagmi';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, provider } = configureChains(
+  [chain.ropsten],
+  [
+    alchemyProvider({ alchemyId: process.env.INFURA_ID }),
+    publicProvider()
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'Alfred',
+  chains
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+})
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Header/>
-    <Routes>
-      <Route path="/" element={<App />}/>
-      <Route path="proposals" element={<Proposals />}/>
-      <Route path="proposals/proposal/:proposalId" element= {<Proposal />}/>
-      <Route path="create-a-proposal" element={<CreateProposal />}/>
-    </Routes>
-  </BrowserRouter>
+  <WagmiConfig client={wagmiClient}>
+    <RainbowKitProvider chains={chains}
+      theme={darkTheme({
+        accentColor: '#fdf6e3',
+        accentColorForeground: '#586e75',
+        borderRadius: 'medium',
+        fontStack: 'system',
+      })}
+    >
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<App />}/>
+          <Route path="proposals" element={<Proposals />}/>
+          <Route path="proposals/proposal/:proposalId" element= {<Proposal />}/>
+          <Route path="create-a-proposal" element={<CreateProposal />}/>
+        </Routes>
+      </BrowserRouter>
+     </RainbowKitProvider>
+   </WagmiConfig>
 );
 
 reportWebVitals();
