fd71cf8e535a1a833d2b535563bfb44752f899ba
Mete Karasakal <mete@Metes-MacBook-Air.local>
2022-06-11 19:06:31 +0200

    proposals web3 fixed

--- front-end/src/components/Proposals.jsx
+++ front-end/src/components/Proposals.jsx
@@ -8,7 +8,7 @@ const Proposals = () => {
   const [proposals, setProposals] = useState([]);
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  useLayoutEffect(()=>{
+  useEffect(()=>{
     const provider = new ethers.providers.JsonRpcProvider("https://ropsten.infura.io/v3/42d2fdb19ea44752b70d96e723a4b829"/*+process.env.INFURA_ID8*/);
     const address = "0xa46ec2049cd1b95617DF848Ef07B61c49c6961CB";
 
@@ -19,24 +19,31 @@ const Proposals = () => {
       return result[0].toNumber();
     }
 
-    async function changeVersions() {
-      const verNum = await changeVersionNumber();
-      if(verNum>proposals.length-1){
-        for(let propID=1; propID <= verNum; propID++){
-          const contract = new ethers.Contract(address, abi, provider);   
-          const result = await contract.functions.changeVersions(propID);
-        
-          setProposals(proposals.concat({
-              id: propID,
-              title: result.name,
-              // title: ethers.utils.parseBytes32String(result.name),
-              content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit mauris nibh, sed faucibus dolor laoreet pellentesque. Fusce orci arcu, semper vitae rutrum nec, dignissim et nunc. In nec massa non dui mattis euismod sit amet eget nulla. Nunc at hendrerit erat, dignissim scelerisque felis. Mauris sit amet sem leo. Vestibulum tincidunt pellentesque sagittis. Etiam cursus non est ut sollicitudin. Vestibulum auctor, nulla auctor pharetra luctus, ipsum nunc mollis massa, non tincidunt lorem urna ut lectus. Vestibulum dapibus purus vitae semper maximus. Cras in libero at lacus porttitor tempus vitae ac metus.',
-              label: 0
-            }));
+    async function changeVersions(verNum) {
+      let contract = new ethers.Contract(address, abi, provider);   
+      let result;
+      let proposalsTemp=[];
+      for(let propID=1; propID <= verNum; propID++){
+        result = await contract.functions.changeVersions(propID);
+        console.log(propID)
+        proposalsTemp.push({
+            id: propID,
+            title: result.name,
+            // title: ethers.utils.parseBytes32String(result.name),
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit mauris nibh, sed faucibus dolor laoreet pellentesque. Fusce orci arcu, semper vitae rutrum nec, dignissim et nunc. In nec massa non dui mattis euismod sit amet eget nulla. Nunc at hendrerit erat, dignissim scelerisque felis. Mauris sit amet sem leo. Vestibulum tincidunt pellentesque sagittis. Etiam cursus non est ut sollicitudin. Vestibulum auctor, nulla auctor pharetra luctus, ipsum nunc mollis massa, non tincidunt lorem urna ut lectus. Vestibulum dapibus purus vitae semper maximus. Cras in libero at lacus porttitor tempus vitae ac metus.',
+            label: 0
+          });
         }
-      }
+        console.log(proposalsTemp)
+        return proposalsTemp;
     }
-    changeVersions();
+    changeVersionNumber().then((verNum) => {
+      if(proposals.length<verNum-1){
+        changeVersions(verNum).then((item) => setProposals(proposals.concat(item)));
+        console.log('out verlog', verNum)
+      }
+    })
+    
     console.log(proposals);
   })
 
@@ -47,10 +54,10 @@ const Proposals = () => {
       <hr className="title-seperator"/>
       {proposals.length > 0 ? proposals.map((proposal) => {
         return(
-          <Link to={`/proposals/proposal/${proposal.id}`}>
-            <div className="proposal-list-view" key={proposal.id}>
+          <Link to={`/proposals/proposal/${proposal.id}`} key={proposal.id}>
+            <div className="proposal-list-view">
               <div className="proposal-title-container">
-                <h2 className="proposal-title">{proposal.title}</h2>
+                <h2 className="proposal-title">{proposal.id}{proposal.title}</h2>
                 <p className={proposal.label ? 'proposal-label-update' : 'proposal-label-revert'}>{proposal.label ? 'Update' : 'Revert'}</p>
               </div>
               <p className="proposal-list-content">{proposal.content.length > 300 ? proposal.content.substring(0,300) + '...' : proposal.content }</p>
