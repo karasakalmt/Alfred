31ffd9ae1f41dc6bfcc8f234819c1565c7294e81
Mete Karasakal <mete@Metes-MacBook-Air.local>
2022-06-11 22:30:01 +0200

    finalizing getting ready for deployment

--- front-end/src/components/Proposals.jsx
+++ front-end/src/components/Proposals.jsx
@@ -7,12 +7,14 @@ import bytes32 from 'bytes32';
 
 const Proposals = () => {
   const [proposals, setProposals] = useState([]);
+  const [latest, setLatestVersion] = useState(0);
+  const [current, setCurrentVersion] = useState(0);
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(()=>{
     const provider = new ethers.providers.JsonRpcProvider("https://ropsten.infura.io/v3/42d2fdb19ea44752b70d96e723a4b829"/*+process.env.INFURA_ID8*/);
     const address = "0xa46ec2049cd1b95617DF848Ef07B61c49c6961CB";
-
+    const proxyAddress = "0x368853C9B1973E328700cE70fe3d2a1ebe6E1De1";
     async function changeVersionNumber() {
       const contract = new ethers.Contract(address, abi, provider);   
       const result = await contract.functions.changeVersionNumber();
@@ -30,7 +32,7 @@ const Proposals = () => {
             id: propID,
             title: bytes32({ input: result.name }),
             content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit mauris nibh, sed faucibus dolor laoreet pellentesque. Fusce orci arcu, semper vitae rutrum nec, dignissim et nunc. In nec massa non dui mattis euismod sit amet eget nulla. Nunc at hendrerit erat, dignissim scelerisque felis. Mauris sit amet sem leo. Vestibulum tincidunt pellentesque sagittis. Etiam cursus non est ut sollicitudin. Vestibulum auctor, nulla auctor pharetra luctus, ipsum nunc mollis massa, non tincidunt lorem urna ut lectus. Vestibulum dapibus purus vitae semper maximus. Cras in libero at lacus porttitor tempus vitae ac metus.',
-            label: 0
+            label: result.version.toNumber()>=current
           });
         }
         console.log(proposalsTemp)
@@ -42,28 +44,55 @@ const Proposals = () => {
         console.log('out verlog', verNum)
       }
     })
-    
+
+    async function latestVersion() {
+      const proxyAbi = [
+        "function latestVersion() view returns (uint256)"
+      ];
+      const contract = new ethers.Contract(proxyAddress, proxyAbi, provider);   
+      const result = await contract.functions.latestVersion();
+      setLatestVersion(result[0].toNumber());
+    }
+    async function currentVersion() {
+      const proxyAbi = [
+        "function currentVersion() view returns (uint256)"
+      ];
+      const contract = new ethers.Contract(proxyAddress, proxyAbi, provider);   
+      const result = await contract.functions.currentVersion();
+      setCurrentVersion(result[0].toNumber());
+    }
+    if(latest === 0){
+      latestVersion();
+    }
+    if(current === 0){
+      currentVersion();
+    }
     console.log(proposals);
   })
+
+
 
 
   return (
     <div className="container">
       <h1 className="title">Proposals</h1>
+      <p>Latest Version: {latest} - Current Version: {current}</p>
       <hr className="title-seperator"/>
       {proposals.length > 0 ? proposals.map((proposal) => {
         return(
           <Link to={`/proposals/proposal/${proposal.id}`} key={proposal.id}>
             <div className="proposal-list-view">
               <div className="proposal-title-container">
-                <h2 className="proposal-title">{proposal.id}{proposal.title}</h2>
+                <h2 className="proposal-title">#{proposal.id} {proposal.title}</h2>
                 <p className={proposal.label ? 'proposal-label-update' : 'proposal-label-revert'}>{proposal.label ? 'Update' : 'Revert'}</p>
               </div>
               <p className="proposal-list-content">{proposal.content.length > 300 ? proposal.content.substring(0,300) + '...' : proposal.content }</p>
             </div>
           </Link>
         )
-      }): <>{proposals.length}</>}
+      }): <>
+        <p className="proposal-list-content">There is no proposal to see.</p>
+      </>}
     </div>
   )
 }
