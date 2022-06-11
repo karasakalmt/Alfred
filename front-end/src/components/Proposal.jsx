b306491ba078d76eab0352d32e830dd39764d99b
Mete Karasakal <mete@Metes-MacBook-Air.local>
2022-06-11 20:49:43 +0200

    vote contract interaction

--- front-end/src/components/Proposal.jsx
+++ front-end/src/components/Proposal.jsx
@@ -1,68 +1,78 @@
 import { CopyBlock, solarizedDark } from "react-code-blocks";
 import abi from '../abis/SimpleDAO.json'
 import { useEffect, useState } from "react";
-import ethers from "ethers";
-const proposalx = {
-  title: 'The Proposal', 
-  content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit mauris nibh, sed faucibus dolor laoreet pellentesque. Fusce orci arcu, semper vitae rutrum nec, dignissim et nunc. In nec massa non dui mattis euismod sit amet eget nulla. Nunc at hendrerit erat, dignissim scelerisque felis. Mauris sit amet sem leo. Vestibulum tincidunt pellentesque sagittis. Etiam cursus non est ut sollicitudin. Vestibulum auctor, nulla auctor pharetra luctus, ipsum nunc mollis massa, non tincidunt lorem urna ut lectus. Vestibulum dapibus purus vitae semper maximus. Cras in libero at lacus porttitor tempus vitae ac metus.',
-  label: 1,
-  code: `pragma solidity >=0.5.0 <0.7.0;
-          contract HelloWorld {
-              function get()public pure returns (string memory){
-                  return 'Hello Contracts';
-              }
-          }`, 
-  address: '0x000000000000000000000000000000000', 
-  bounty: 0, 
-  vote: {
-    yes: 0,
-    no: 0
-  }, 
-  discussion: [
-    {
-      address:'0x000000000000000000000000000000000',
-      comment:'It looks awesome.'
-    },
-    {
-      address:'0x000000000000000000000000000000000',
-      comment:'This is very bad.'
-    }
-  ]
-}
+import { ethers } from "ethers";
+import bytes32 from "bytes32";
+import { useParams } from "react-router-dom";
 
 const Proposal = () => {
-  const [proposal, setProposal] = useState({});
-  // // eslint-disable-next-line react-hooks/exhaustive-deps
-  // useEffect(async ()=>{
-  //   const provider = new ethers.providers.JsonRpcProvider("https://ropsten.infura.io/v3/42d2fdb19ea44752b70d96e723a4b829"/*+process.env.INFURA_ID8*/);
-  //   const address = "0xa46ec2049cd1b95617DF848Ef07B61c49c6961CB";
+  let {proposalId} = useParams();
+  const [proposal, setProposal] = useState(null);
+  // const provider = new ethers.providers.JsonRpcProvider("https://ropsten.infura.io/v3/42d2fdb19ea44752b70d96e723a4b829"/*+process.env.INFURA_ID8*/);
+  const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+  const address = "0xa46ec2049cd1b95617DF848Ef07B61c49c6961CB";
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(()=>{
 
-  //   async function changeVersionNumber() {
-  //     const contract = new ethers.Contract(address, abi, provider);   
-  //     const result = await contract.functions.changeVersionNumber();
-
-  //     return result[0].toNumber();
-  //   }
-
-  //   async function changeVersions(ver) {
-  //     const contract = new ethers.Contract(address, abi, provider);   
-  //     const result = await contract.functions.changeVersions(ver);
+    async function changeVersions(ver) {
+      const contract = new ethers.Contract(address, abi, provider);   
+      const result = await contract.functions.changeVersions(ver);
     
-  //     setProposal(
-  //       proposal = {
-  //         id: ver,
-  //         title: ethers.utils.parseBytes32String(result.name),
-  //         content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit mauris nibh, sed faucibus dolor laoreet pellentesque. Fusce orci arcu, semper vitae rutrum nec, dignissim et nunc. In nec massa non dui mattis euismod sit amet eget nulla. Nunc at hendrerit erat, dignissim scelerisque felis. Mauris sit amet sem leo. Vestibulum tincidunt pellentesque sagittis. Etiam cursus non est ut sollicitudin. Vestibulum auctor, nulla auctor pharetra luctus, ipsum nunc mollis massa, non tincidunt lorem urna ut lectus. Vestibulum dapibus purus vitae semper maximus. Cras in libero at lacus porttitor tempus vitae ac metus.',
-  //         label: 0
-  //       });
-  //   }
-  //   for(let propID=1; propID <= await changeVersionNumber(); propID++){
-  //     await changeVersions(propID);
-  //   }
-  //   console.log(proposals)
-  // })
+      setProposal({
+          id: ver,
+          title: bytes32({ input: result.name }),
+          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit mauris nibh, sed faucibus dolor laoreet pellentesque. Fusce orci arcu, semper vitae rutrum nec, dignissim et nunc. In nec massa non dui mattis euismod sit amet eget nulla. Nunc at hendrerit erat, dignissim scelerisque felis. Mauris sit amet sem leo. Vestibulum tincidunt pellentesque sagittis. Etiam cursus non est ut sollicitudin. Vestibulum auctor, nulla auctor pharetra luctus, ipsum nunc mollis massa, non tincidunt lorem urna ut lectus. Vestibulum dapibus purus vitae semper maximus. Cras in libero at lacus porttitor tempus vitae ac metus.',
+          label: 0,
+          code: `pragma solidity >=0.5.0 <0.7.0;
+                  contract HelloWorld {
+                      function get()public pure returns (string memory){
+                          return 'Hello Contracts';
+                      }
+                  }`, 
+          address: '0x000000000000000000000000000000000', 
+          bounty: 0, 
+          vote: {
+            yes: result.yes.toNumber(),
+            no: result.no.toNumber()
+          }, 
+          discussion: [
+            {
+              id:1,
+              address:'0x000000000000000000000000000000000',
+              comment:'It looks awesome.'
+            },
+            {
+              id:2,
+              address:'0x000000000000000000000000000000000',
+              comment:'This is very bad.'
+            }
+          ]
+        });
+    }
+    if(proposal==null){
+    changeVersions(proposalId);
+  }
+    console.log(proposal)
+  })
+
+  async function voteUpgradeToProposal(lord) {
+    await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(address, abi, signer);   
+    const tx = await contract.functions.voteUpgradeToProposal(proposalId,lord);
+  
+    const receipt = await tx.wait();
+    console.log("receipt", receipt);
+    if(lord){
+      setProposal({vote: {yes: proposal.vote.yes+1, no: proposal.vote.no},...proposal})
+    } else{
+      setProposal({vote: {yes: proposal.vote.yes, no: proposal.vote.no+1},...proposal})
+    }
+  }
+
   return (
-    <div className="container">
+    <>
+    {proposal != null ? <div className="container">
       <div className="proposal-title-container">
         <h1 className="title">{proposal.title}</h1>
         <p className={proposal.label ? 'proposal-label-update' : 'proposal-label-revert'}>{proposal.label ? 'Update' : 'Revert'}</p>
@@ -94,8 +104,8 @@ const Proposal = () => {
       <div className="proposal-list-view">
         <h2 className="proposal-title">Vote</h2> 
         <div className="vote-container">
-            <button className="vote-yes">Yes</button>
-            <button className="vote-no">No</button>
+            <button className="vote-yes" onClick={()=>voteUpgradeToProposal(true)}>Yes: {proposal.vote.yes}</button>
+            <button className="vote-no" onClick={()=>voteUpgradeToProposal(false)}>No: {proposal.vote.no}</button>
         </div>
         <hr className="title-seperator"/>
         <h2 className="proposal-title" style={{margin: '10px 0px'}}>Bounty</h2>
@@ -118,7 +128,7 @@ const Proposal = () => {
         </form>
       {proposal.discussion.length>0 ? <>
         {proposal.discussion.map((discussion) => {
-            return(<div style={{margin: '10px 0px'}}>
+            return(<div style={{margin: '10px 0px'}} key={discussion.id}>
             <hr className="title-seperator"/>
             <p className="proposal-list-content" style={{margin: '10px 0px'}}>{discussion.address}</p>
             <p className="proposal-list-content" style={{margin: '10px 0px'}}>{discussion.comment}</p>
@@ -127,10 +137,9 @@ const Proposal = () => {
         }
         </>: ''}
       </div>
-    </div>
+    </div>:''}
+    </>
   )
 }
-
-//- Proposal detail page (title, content, code, team address, bounty, vote, discussion)
 
 export default Proposal;
