5126774945e381a92110b5c1ce0a143f5d23f32a
Mete Karasakal <mete@Metes-MacBook-Air.local>
2022-06-11 18:12:08 +0200

    proposals web3 buggy

--- front-end/src/components/CreateProposal.jsx
+++ front-end/src/components/CreateProposal.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { useState } from 'react'
 
 const CreateProposal = () => {
+  const [title, setTitle] = useState('');
+  const [address, setAddress] = useState('');
+  const [content, setContent] = useState('');
+  const [type, setType] = useState('');
+
   return (
     <div className="container" style={{width: '50%'}}>
       <div className="proposal-title-container">
@@ -9,12 +15,21 @@ const CreateProposal = () => {
       <hr className="title-seperator"/>
       <div className='proposal-list-view'>
         <label for="proposal-title">Title</label>
-        <input className="proposal-text-box" type="text" name="proposal-title"/>
-        <label for="proposal-title">GitHub/IPFS URL</label>
-        <input className="proposal-text-box" type="text" name="proposal-title"/>
+        <input onChange={(e) => setTitle(e.target.value)} className="proposal-text-box" type="text" name="proposal-title"/>
+        <label for="proposal-title">Contract Address/Version ID</label>
+        <input onChange={(e) => setAddress(e.target.value)} className="proposal-text-box" type="text" name="proposal-title"/>
+        <label for="proposal-title">Proposal Type</label>
+        <div onChange={(e) => setType(e.target.value)} className="radio-box">
+          <span>
+            <input type="radio" value="update" name="status" /> Update
+          </span> 
+          <span>
+            <input type="radio" value="revert" name="status" /> Revert
+          </span>
+        </div>
         <label style={{margin: '0px 10px 0px 0px'}}>
           Content
-          <textarea></textarea>
+          <textarea onChange={(e) => setContent(e.target.value)}></textarea>
         </label>
         <input className="submit-button" style={{backgroundColor:'#859900'}} type="submit" value="Create Proposal" />
       </div>
--- front-end/src/components/Proposals.jsx
+++ front-end/src/components/Proposals.jsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import abi from '../abis/SimpleDAO.json'
+// require('dotenv').config();
 
-const proposals = [
+const proposalsx = [
   {
     id:1,
     title: 'Proposal #1', 
@@ -40,11 +44,45 @@ const proposals = [
 ]
 
 const Proposals = () => {
+  const [proposals, setProposal] = useState([]);
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(async ()=>{
+    const provider = new ethers.providers.JsonRpcProvider("https://ropsten.infura.io/v3/42d2fdb19ea44752b70d96e723a4b829"/*+process.env.INFURA_ID8*/);
+    const address = "0xa46ec2049cd1b95617DF848Ef07B61c49c6961CB";
+
+    async function changeVersionNumber() {
+      const contract = new ethers.Contract(address, abi, provider);   
+      const result = await contract.functions.changeVersionNumber();
+
+      return result[0].toNumber();
+    }
+
+    async function changeVersions(ver) {
+      const contract = new ethers.Contract(address, abi, provider);   
+      const result = await contract.functions.changeVersions(ver);
+    
+      setProposal(
+        proposals.push({
+          id: ver,
+          title: ethers.utils.parseBytes32String(result.name),
+          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit mauris nibh, sed faucibus dolor laoreet pellentesque. Fusce orci arcu, semper vitae rutrum nec, dignissim et nunc. In nec massa non dui mattis euismod sit amet eget nulla. Nunc at hendrerit erat, dignissim scelerisque felis. Mauris sit amet sem leo. Vestibulum tincidunt pellentesque sagittis. Etiam cursus non est ut sollicitudin. Vestibulum auctor, nulla auctor pharetra luctus, ipsum nunc mollis massa, non tincidunt lorem urna ut lectus. Vestibulum dapibus purus vitae semper maximus. Cras in libero at lacus porttitor tempus vitae ac metus.',
+          label: 0
+        })
+      );
+    }
+    for(let propID=1; propID <= await changeVersionNumber(); propID++){
+      await changeVersions(propID);
+    }
+    console.log(proposals)
+  })
+
+
   return (
     <div className="container">
       <h1 className="title">Proposals</h1>
       <hr className="title-seperator"/>
-      {proposals.map((proposal) => {
+      {proposals.length > 0 ? proposals.map((proposal) => {
         return(
           <Link to={`/proposals/proposal/${proposal.id}`}>
             <div className="proposal-list-view" key={proposal.id}>
@@ -56,7 +94,7 @@ const Proposals = () => {
             </div>
           </Link>
         )
-      })}
+      }): <>{proposals.length}</>}
     </div>
   )
 }
